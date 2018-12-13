d649fed136e6a8d23a37cd6e8086986cd67d034d
liweiguang <851989831@qq.com>
2018-12-13 21:29:30 +0800

    Latency

--- ParserJson.py
+++ ParserJson.py
@@ -4,11 +4,12 @@ import os
 from excelTable import excelTable
 from fileData import fileData
 
-def run_bw(dirname):
-    target = 'bw'
+def run_latency(dirname):
+    target = 'latency'
     bss = []
     numjobs = []
     depths = []
+    iopss = []
     for root, dirs, files in os.walk(dirname):
         # print(root)  # os.walk()所在目录
         # print(dirs)  # os.walk()所在目录的所有目录名
@@ -21,16 +22,19 @@ def run_bw(dirname):
             bss.insert(0, int(tmp[1]))
             numjobs.insert(0, int(tmp[2]))
             depths.insert(0, int(tmp[3]))
+            iopss.insert(0, int(tmp[4]))
     bss = list(set(bss))
     bss.sort()
     numjobs = list(set(numjobs))
     numjobs.sort()
     depths = list(set(depths))
     depths.sort()
+    iopss = list(set(iopss))
+    iopss.sort()
     print(bss, numjobs, depths)
 
     a = excelTable(dirname + '/' + target + '.xls')
-    a.setFrame(numjobs, depths, bss)
+    a.setFrame(numjobs, depths, bss, iopss)
     for root, dirs, files in os.walk(dirname):
         for file in files:
             rw = -1
@@ -45,65 +49,22 @@ def run_bw(dirname):
             bs = int(tmp[1])
             numjob = int(tmp[2])
             depth = int(tmp[3])
+            iops = int(tmp[4])
             data = fileData(dirname + '/' + file)
             if rw == 0:
-                bw = data.getReadBw()
+                latency_mean = data.getReadClatencyMean()
+                latency_95 = data.getReadClatency95()
+                latency_9999 = data.getReadClatency9999()
             else:
-                bw = data.getWriteBw()
-            a.setValue(a.getPositionFromJson({'rw': rw, 'numjob': numjob, 'depth': depth, 'bs': bs}), bw)
-            data.f.close()
-
-def run_iops(dirname):
-    target = 'iops'
-    bss = []
-    numjobs = []
-    depths = []
-    for root, dirs, files in os.walk(dirname):
-        # print(root)  # os.walk()所在目录
-        # print(dirs)  # os.walk()所在目录的所有目录名
-        # print(files)  # os.walk()所在目录的所有非目录文件名
-        for file in files:
-            if not file.startswith('r_') and not file.startswith('w_'):
-                continue
-            # print(file)
-            tmp = file.split('_')
-            bss.insert(0, int(tmp[1]))
-            numjobs.insert(0, int(tmp[2]))
-            depths.insert(0, int(tmp[3]))
-    bss = list(set(bss))
-    bss.sort()
-    numjobs = list(set(numjobs))
-    numjobs.sort()
-    depths = list(set(depths))
-    depths.sort()
-    print(bss, numjobs, depths)
-
-    a = excelTable(dirname + '/' + target + '.xls')
-    a.setFrame(numjobs, depths, bss)
-    for root, dirs, files in os.walk(dirname):
-        for file in files:
-            rw = -1
-            if file.startswith('r_'):
-                rw = 0
-            elif file.startswith('w_'):
-                rw = 1
-            else:
-                continue
-            print(file)
-            tmp = file.split('_')
-            bs = int(tmp[1])
-            numjob = int(tmp[2])
-            depth = int(tmp[3])
-            data = fileData(dirname + '/' + file)
-            if rw == 0:
-                iops = data.getReadIops()
-            else:
-                iops = data.getWriteIops()
-            a.setValue(a.getPositionFromJson({'rw': rw, 'numjob': numjob, 'depth': depth, 'bs': bs}), iops)
+                latency_mean = data.getWriteClatencyMean()
+                latency_95 = data.getWriteClatency95()
+                latency_9999 = data.getWriteClatency9999()
+            a.setValue(a.getPositionFromJson({'rw': rw, 'numjob': numjob, 'depth': depth, 'bs': bs, 'horizontal': iops, 'vertical':'mean'}), latency_mean)
+            a.setValue(a.getPositionFromJson({'rw': rw, 'numjob': numjob, 'depth': depth, 'bs': bs, 'horizontal': iops, 'vertical': '95.00%'}),latency_95)
+            a.setValue(a.getPositionFromJson({'rw': rw, 'numjob': numjob, 'depth': depth, 'bs': bs, 'horizontal': iops, 'vertical': '99.99%'}),latency_9999)
             data.f.close()
 
 if __name__ == '__main__':
-    run_bw('fio-bandwidth-iops-core')
-    run_iops('fio-bandwidth-iops-core')
+    run_latency('fio-latency-32')
 
 
--- excelTable.py
+++ excelTable.py
@@ -19,29 +19,41 @@ class excelTable(object):
         self.style.alignment = al
         self.workbook.save(excelFileName)
 
-    def setFrame(self, numjobs, depths, bss):
+    def setFrame(self, numjobs, depths, bss, iops):
+        self.bss = bss
         self.numjobs = numjobs
         self.depths = depths
-        self.bss = bss
+        self.iops = iops
+        bs_length = len(bss)
         numjob_length = len(numjobs)
         depth_length = len(depths)
-        bs_length = len(bss)
-        rws = ('read','write')
-        for rw_index in range(len(rws)):
-            rw_position = {'x': rw_index * numjob_length * ( 2 + bs_length ) + 1, 'y': 0} # x towords down ; y towords the right
-            self.worksheet.write_merge(rw_position['x'], rw_position['x'] + numjob_length * ( 2 + bs_length ) - 1, rw_position['y'],rw_position['y'], rws[rw_index], self.style)
-            for numjob_index in range(len(numjobs)):
-                numjobs_position = {'x': rw_position['x'] + numjob_index * ( 2 + bs_length ), 'y': rw_position['y'] + 1}
-                depths_position = {'x': numjobs_position['x'], 'y': numjobs_position['y'] + 4}
-                bss_position = {'x': numjobs_position['x'] + 2, 'y': numjobs_position['y'] + 2}
-                self.worksheet.write_merge(numjobs_position['x'],numjobs_position['x'] + bs_length + 1,numjobs_position['y'],numjobs_position['y'],"numjobs=" + str(numjobs[numjob_index]), self.style)
-                self.worksheet.write_merge(depths_position['x'],depths_position['x'],depths_position['y'],depths_position['y'] + depth_length - 1,"depths", self.style)
-                self.worksheet.write_merge(bss_position['x'],bss_position['x'] + bs_length - 1,bss_position['y'],bss_position['y'],"bs", self.style)
+        iops_length = len(iops)
 
-                for i in range(len(depths)):
-                    self.worksheet.write(depths_position['x'] + 1, depths_position['y'] + i, depths[i], self.style)
-                for i in range(len(bss)):
-                    self.worksheet.write(bss_position['x'] + i, bss_position['y'] + 1, bss[i], self.style)
+        rws = ('read','write')
+        # for rw_index in range(len(rws)):
+        #     rw_position = {'x': rw_index * numjob_length * ( 2 + bs_length ) + 1, 'y': 0} # x towords down ; y towords the right
+        #     self.worksheet.write_merge(rw_position['x'], rw_position['x'] + numjob_length * ( 2 + bs_length ) - 1, rw_position['y'],rw_position['y'], rws[rw_index], self.style)
+        #     for numjob_index in range(len(numjobs)):
+        #         numjobs_position = {'x': rw_position['x'] + numjob_index * ( 2 + bs_length ), 'y': rw_position['y'] + 1}
+        #         depths_position = {'x': numjobs_position['x'], 'y': numjobs_position['y'] + 4}
+        #         bss_position = {'x': numjobs_position['x'] + 2, 'y': numjobs_position['y'] + 2}
+        #         self.worksheet.write_merge(numjobs_position['x'],numjobs_position['x'] + bs_length + 1,numjobs_position['y'],numjobs_position['y'],"numjobs=" + str(numjobs[numjob_index]), self.style)
+        #         self.worksheet.write_merge(depths_position['x'],depths_position['x'],depths_position['y'],depths_position['y'] + depth_length - 1,"depths", self.style)
+        #         self.worksheet.write_merge(bss_position['x'],bss_position['x'] + bs_length - 1,bss_position['y'],bss_position['y'],"bs", self.style)
+        #
+        #         for i in range(len(depths)):
+        #             self.worksheet.write(depths_position['x'] + 1, depths_position['y'] + i, depths[i], self.style)
+        #         for i in range(len(bss)):
+        #             self.worksheet.write(bss_position['x'] + i, bss_position['y'] + 1, bss[i], self.style)
+        for rw_index in range(len(rws)):
+            rw_position = {'x': rw_index * 5 + 1,'y': 0}
+            self.worksheet.write_merge(rw_position['x'], rw_position['x'] + 4, rw_position['y'], rw_position['y'], rws[rw_index], self.style)
+            self.worksheet.write(rw_position['x'] + 2, rw_position['y'] + 1, 'mean', self.style)
+            self.worksheet.write(rw_position['x'] + 3, rw_position['y'] + 1, '95.00%', self.style)
+            self.worksheet.write(rw_position['x'] + 4, rw_position['y'] + 1, '99.99%', self.style)
+            self.worksheet.write_merge(rw_position['x'], rw_position['x'], rw_position['y'] + 2, rw_position['y'] + 2 + iops_length - 1, 'IOPS', self.style)
+            for i in range(len(iops)):
+                self.worksheet.write(rw_position['x'] + 1, rw_position['y'] + 2 + i, iops[i], self.style)
         self.workbook.save(self.excelFileName)
 
     def getPositionFromJson(self, json):
@@ -49,38 +61,37 @@ class excelTable(object):
         numjob = json['numjob']
         depth = json['depth']
         bs = json['bs']
-        if numjob not in self.numjobs:
+        horizontal = json['horizontal']
+        vertical = json['vertical']
+        if horizontal not in self.iops:
             return -1
-        if depth not in self.depths:
+        verticals = ('mean', '95.00%', '99.99%')
+        if vertical not in verticals:
             return -1
-        if bs not in self.bss:
-            return -1
-        numjob_index = -1
-        depth_index = -1
-        bs_index = -1
-        for numjob_index in range(len(self.numjobs)):
-            if self.numjobs[numjob_index] == numjob:
+        horizontal_index = -1
+        vertical_index = -1
+        for vertical_index in range(len(verticals)):
+            if verticals[vertical_index] == vertical:
                 break
-        numjob_length = len(self.numjobs)
-        depth_length = len(self.depths)
-        bs_length = len(self.bss)
-        rw_position = {'x': rw * numjob_length * (2 + bs_length) + 1, 'y': 0}
-        numjob_position = {'x': rw_position['x'] + numjob_index * (2 + bs_length), 'y': rw_position['y'] + 1}
-        depth_position = {'x': numjob_position['x'], 'y': numjob_position['y'] + 4}
-        bs_position = {'x': numjob_position['x'] + 2, 'y': numjob_position['y'] + 2}
+        for horizontal_index in range(len(self.iops)):
+            if self.iops[horizontal_index] == horizontal:
+                break
+
+        rw_position = {'x': rw * 5 + 1,'y': 0}
+        table_position = {'x': rw_position['x'], 'y': rw_position['y'] + 1}
 
         ExcelFile = xlrd.open_workbook(self.excelFileName)
         table = ExcelFile.sheets()[0]
-        for i in range(len(self.depths)):
-            if table.cell(depth_position['x'] + 1,depth_position['y'] + i).value == depth:
-                depth_index = i
+        for i in range(len(self.iops)):
+            if table.cell(table_position['x'] + 1,table_position['y'] + 1 + i).value == horizontal:
+                horizontal_index = i
                 break
         for i in range(len(self.depths)):
-            if table.cell(bs_position['x'] + i,bs_position['y'] + 1).value == bs:
-                bs_index = i
+            if table.cell(table_position['x'] + 2 + i,table_position['y']).value == vertical:
+                vertical_index = i
                 break
-        x = depth_position['x'] + 2 + bs_index
-        y = depth_position['y'] + depth_index
+        x = table_position['x'] + 2 + vertical_index
+        y = table_position['y'] + 1 + horizontal_index
         position = {}
         position['x'] = x
         position['y'] = y
@@ -101,11 +112,13 @@ class excelTable(object):
         a.setValue(a.getPositionFromJson({'numjob': 4, 'depth': 32, 'bs': 4}), 'lwg')
 
 if __name__ == '__main__':
-    data = fileData('r_1024_4_32_1')
-    bw = data.getReadBw()
+    # data = fileData('r_1024_4_32_1')
+    # bw = data.getReadBw()
     a = excelTable("2.xls")
-    numjobs = (1, 2, 4)
-    depths = (1, 32, 64)
-    bss = (4, 1024, 2048)
-    a.setFrame(numjobs, depths, bss)
-    a.setValue(a.getPositionFromJson({'numjob': 4, 'depth': 32, 'bs': 1024}), bw)
+    bss = [4]
+    numjobs = (1, 4)
+    depths = (1, 32)
+    iops = (100, 1000, 2500, 7500)
+    a.setFrame(numjobs, depths, bss, iops)
+    print(a.getPositionFromJson({'rw':0, 'numjob': 4, 'depth': 32, 'bs': 1024, 'horizontal':2500, 'vertical':'99.99%'}))
+    # a.setValue(a.getPositionFromJson({'numjob': 4, 'depth': 32, 'bs': 1024}), bw)
